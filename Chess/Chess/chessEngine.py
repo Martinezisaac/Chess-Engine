e6464d8a7f93669bc6154c38e72f36e4aa13b7a8
Isaac Martínez <isaac.martinez6437@alumnos.udg.mx>
2024-06-27 23:14:21 -0600

    Realizar jugadas en el tablero

--- Chess/Chess/chessEngine.py
+++ Chess/Chess/chessEngine.py
@@ -23,7 +23,42 @@ class gameState():
             ['--', '--', '--', '--', '--', '--', '--', '--'], #Fila vacia 
             ['--', '--', '--', '--', '--', '--', '--', '--'], #Fila vacia 
             ['--', '--', '--', '--', '--', '--', '--', '--'], #Fila vacia 
-            ['--', '--', '--', '--', '--', '--', '--', '--'],#Fila vacia
+            ['--', '--', '--', '--', '--', '--', '--', '--'], #Fila vacia
             ['wPawn', 'wPawn', 'wPawn', 'wPawn', 'wPawn', 'wPawn', 'wPawn', 'wPawn'], #Fila de peones blancos
             ['wRook','wKnight', 'wBishop', 'wQueen', 'wKing', 'wBishop', 'wKnight', 'wRook'], #Fila de piezas principales blancos 
         ] #Fin de la lista de listas
+
+        self.movimientoBlancas = True
+        self.historialMovimientos = [] #Lista auxiliar para almacenar el historial de los movimientos 
+
+    def hacerJugada(self, jugada):
+        self.tablero[jugada.filaInicial][jugada.columnaInicial] = "--"
+        self.tablero[jugada.filaFinal][jugada.columnaFinal] = jugada.piezaMovida #El tablero se actualiza con la pieza movida 
+        self.historialMovimientos.append(jugada) #Guardar la jugada en el historial de movimientos relaizadas 
+        self.movimientoBlancas = not self.movimientoBlancas #El movimiento de las blancas no puede ser su mismo movimiento, deben de ser diferentes 
+class Movimiento():
+    # La clase Movimiento almacenara los movimiento y las piezas del tablero
+
+    #Notacion del juego | Diccionarios para crear la notacion 
+    rangosToFilas = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0} #Obtener las filas 
+    filasToRangos = {v: k for k, v in rangosToFilas.items()} #Crear diccionario con los pares clave-valor
+    # Donde v = posiciones del tablero de ajedrez y k = posiciones de la matriz del tablero 
+
+    filasToColumnas = {"a": 0, "b": 1, "c":2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7} #Obtener las columans 
+    columnasToFilas = {v: k for k, v in filasToColumnas.items()} #Crear diccionario con los pares clave-valor
+    # Donde v = posiciones del tablero de ajedrez y k = posiciones de la matriz del tablero
+
+    def __init__(self, cuadroInicial, cuadroFinal, tablero):
+        self.filaInicial = cuadroInicial[0] #Tomar el primer elemento (fila) del cuadro del primer clic 
+        self.columnaInicial = cuadroInicial[1] #Tomar el segundo elemento (columna) del cuadro del primer clic
+        self.filaFinal = cuadroFinal[0] #Tomar el primer elemento (fila) del cuadro del segundo clic 
+        self.columnaFinal = cuadroFinal[1] #Tomar el segundo elemento (columna) del segundo clic 
+        self.piezaMovida = tablero[self.filaInicial][self.columnaInicial] #Posicion inicial de la pieza a mover 
+        self.piezaCapturada = tablero[self.filaFinal][self.columnaFinal] #Posicion final de la pieza movida
+
+    def obtenerNotacion(self): #Obtener la notacion de ajedrez 
+        return self.obtenerRangoFila(self.filaInicial, self.columnaInicial) + self.obtenerRangoFila(self.filaFinal, self.columnaFinal)
+
+    def obtenerRangoFila(self, fila, columna): #Obtener la posicion de la pieza
+        return self.columnasToFilas[columna] + self.filasToRangos[fila] #A4, B7, H6, B4... 
+
--- Chess/chessMain.py
+++ Chess/chessMain.py
@@ -18,7 +18,7 @@ def cargarImagenes():
     piezas = ['bPawn', 'bKnight', 'bBishop', 'bRook', 'bQueen', 'bKing', 'wPawn', 'wKnight', 'wBishop', 'wRook', 'wQueen', 'wKing'] #Lista con el nombre de las piezas dentro de la carpeta "imagenesPiezas"
 
     for pieza in piezas: #Iterar las imagenes dentro de la carpeta "imagenesPiezas"
-        imagenes[pieza] = p.transform.scale(p.image.load("Chess/imagenesPiezas/" + pieza + ".png"), (tamañoCuadros, tamañoCuadros)) #Cargar cada una de las imagenes de las piezas del juego 
+        imagenes[pieza] = p.transform.scale(p.image.load("Chess/Chess/imagenesPiezas/" + pieza + ".png"), (tamañoCuadros, tamañoCuadros)) #Cargar cada una de las imagenes de las piezas del juego 
         #De igual manera se realiza un escalado de imagenes 
 
 #Dibujar los cuadros del tablero
@@ -28,6 +28,7 @@ def dibujarTablero(pantalla):
     colorBlanco = p.Color(238,239,233)
     colorNegro = p.Color(37, 111, 135)
     colores = [p.Color(colorBlanco), p.Color(colorNegro)] #Definir los colores para el tablero
+
     for fila in range(dimension): #Filas del tablero 
         for columna in range(dimension): #Columnas del tablero 
             color = colores[((columna + fila) % 2)] #El color se obtiene calculando la posicion del cuadro dentro del tablero
@@ -52,22 +53,59 @@ def dibujarEstadoJuego(pantalla, estadoJuego):
 
 #Funcion principal
 def main():
+    #Inicializar el tablero 
     p.init() #Inicializar la libreria 
     pantalla = p.display.set_mode((anchura, altura)) #Definir el tamaño de la pantalla 
     reloj = p.time.Clock()
     pantalla.fill(p.Color("White"))
     estadoJuego = chessEngine.gameState() #Acceder a la clase "gameState" que esta dentro del archivo "chessEngine" 
+    #GameState es una clase en donde contiene la ubicacion del tablero del juego como un objeto, siendo este una matriz 
     cargarImagenes() #Cargar imagenes 
     print(estadoJuego.tablero)
 
     #Variables auxiliares
     bandera = True #Variable auxiliar para el while
+    cuadroSeleccionado = () #Tupla que contendra (fila,columna) del cuadro seleccionado por el usuario
+    movimientoJugador = [] #Lista para registrar los clics del usuario para realizar la siguiente jugada, por lo tanto [(fila1, columna1), (fila2, columna2)]
 
     while bandera: #Ejecutar mientras que sea verdadero
         for x in p.event.get(): #Esperar a obtener un evento 
+
+            #Evento de Mouse
+            #Se obtiene la reaccion del mouse del usuario para empezar a validar movimientos dentro del juego o tablero  
+
             if x.type == p.QUIT: #Si el evento es QUIT
                 bandera = False #Salir del ciclo 
-        
+            elif x.type == p.MOUSEBUTTONDOWN: #Entonces se detecto un clic en el tablero 
+                ubicacionMouse = p.mouse.get_pos() #Obtener las coordenadas (x,y) del mouse 
+                fila = ubicacionMouse[1] // tamañoCuadros
+                columna = ubicacionMouse[0] // tamañoCuadros
+                # cuadroSeleccionado = (fila, columna) #Almacenar la fila y columna del cuadro seleccionado 
+
+                #Validar jugadas
+                # Inicialmente se valida si un cuadro es presionado dos veces de manera consecutiva
+                    # Si no es el caso, entonces se guarda el cuadro que se presiono primero y el cuadro que se presiono al ultimo
+                # Finalmente se valida si el usuario realizo dos clics diferentes
+                    # Si ese es el caso, entonces se realiza la jugada 
+
+                if cuadroSeleccionado == (fila, columna): #Validar si el usuario presiono el mismo cuadro dos veces
+                    # print("Jugada no válida...") #Mensaje de confirmacion al usuario 
+                    cuadroSeleccionado = () #Mantener el cuadro seleccionado vacio
+                    movimientoJugador = [] #Mantener los clics vacios para no registrar una jugada y permanecer el tablero 
+                else: #Entonces los dos clics del usuario no son del mismo cuadro 
+                    cuadroSeleccionado = (fila, columna) #Guardar la fila y columna del cuadro 
+                    movimientoJugador.append(cuadroSeleccionado) #Agregar a la lista los clics realizados por el usuario (1er y 2do clic)
+                    # print(cuadroSeleccionado) #Impresion del clic por parte del usuario 
+
+                if len(movimientoJugador) == 2: #Si el usuario realizo exactamente dos clics
+                    jugada = chessEngine.Movimiento(movimientoJugador[0], movimientoJugador[1], estadoJuego.tablero) #Obtener los dos clics realizados por el usuario 
+                    print(jugada.obtenerNotacion()) #Imprimir la notacion de la jugada realizada
+                    estadoJuego.hacerJugada(jugada) #Realizar la jugada 
+                    
+                    #Una vez que la jugada se realizo, las variables se reinician para poder realizar una jugada nuevamente 
+                    cuadroSeleccionado = () #Reiniciar cuadro Seleccionado
+                    movimientoJugador = [] #Reiniciar el movimiento del jugador 
+
         dibujarEstadoJuego(pantalla, estadoJuego) #Dibujar el estado actual del juego 
         reloj.tick(maxFPS) #El juego no se ejecuta a mas de "maxFPS" fotogramas por segundo 
         p.display.flip() #Doble buffering, se intercambian los buffers, mostrando el buffer fuera de la pantalla en la pantalla y ocultando el buffer previamente visible.
